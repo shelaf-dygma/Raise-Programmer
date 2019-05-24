abe33d7e5e27b633f2255a2bbe99eb2a077b3aa4
matt venn <matt@mattvenn.net>
2019-05-24 10:33:08 +0200

    program Raise Huble with bossac

---
+++ README.md
@@ -0,0 +1,34 @@
+# Raise programmer
+
+This programmer is for Linux command line.
+
+## About
+
+The Huble contains a SAMD Arm 21 chip from Atmel. This can be programmed with the bossa programmer once
+the chip is in 'bootloader mode'.
+
+For security reasons, it can only be put in bootloader mode with your help:
+
+* double pressing the reset button.
+* pressing the escape or delete key after a software controlled reset.
+
+This programmer will attempt to find a plugged in Huble, reset it via serial and then program the new firmware.
+
+## Install requirements
+
+Install a new version of bossac.
+
+    git clone https://github.com/shumatech/BOSSA
+    cd BOSSA
+    make
+    sudo cp bin/bossac /usr/local/bin
+
+## Get new firmware
+
+Put the new firmware in this directory, and make sure it's called Raise-Firmware.ino.bin
+
+## Program the firmware
+
+Run the programmer and follow the instructions. You will be promtpted to press the Escape key:
+
+    ./program.py
---
+++ program.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import serial.tools.list_ports
+import time, subprocess, serial
+
+BOOTLOADER_VIDPID = '1209:2200'
+RAISE_VIDPID = '1209:2201'
+
+def reset_w_baud(baud=1200):
+    ports = serial.tools.list_ports.grep(RAISE_VIDPID) # find the right port
+    for port in ports:
+        print("found %s port on %s" % (port.usb_description(), port.device))
+        print("%d reset" % baud)
+        serial.Serial(port=port.device, baudrate=baud)
+        return True
+    else:
+        return False
+
+def program_firmware():
+    ports = serial.tools.list_ports.grep(BOOTLOADER_VIDPID) # find the right port
+    for port in ports:
+        print("found %s port on %s" % (port.usb_description(), port.device))
+        print("programming firmware")
+        import os
+        os.system('bossac -i -d --port %s -e -o 0x2000 -w ./Raise-Firmware.ino.bin -R' % port.device)
+        return True
+    else:
+        return False
+
+# maybe already in bootloader mode
+if program_firmware():
+    exit(0)
+
+# otherwise, prompt for user to press the special key
+raw_input("when the Huble light goes out, press the 'esc' key. Press enter to continue")
+if reset_w_baud():
+    time.sleep(3)
+    program_firmware()
+else:
+    print("couldn't find Dygma Raise")
